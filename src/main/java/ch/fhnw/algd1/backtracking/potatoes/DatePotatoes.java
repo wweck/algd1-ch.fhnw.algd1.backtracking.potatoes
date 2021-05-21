363ef92148f3bf619db2c57e5adfccf921728ec1
Wolfgang Weck <wolfgang.weck@home>
2021-05-21 12:05:05 +0200

    initial commit

---
+++ src/main/java/ch/fhnw/algd1/backtracking/potatoes/DatePotatoes.java
@@ -0,0 +1,25 @@
+package ch.fhnw.algd1.backtracking.potatoes;
+
+public class DatePotatoes {
+
+	private static int[] weights = new int[] { 10, 5, 2, 1, 5, 10 };
+	
+	private static int minDifference = Integer.MAX_VALUE;
+
+	private static void solve() {
+		//TODO Implement
+	}
+
+
+	private static void printResult() {
+		System.out.println("Minimal weight difference is: "+ minDifference);
+	}
+	
+	public static void main(String[] args) {
+		solve();
+		printResult();
+	}
+
+
+
+}
